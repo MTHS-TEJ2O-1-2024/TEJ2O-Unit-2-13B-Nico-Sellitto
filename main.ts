3d2fa148de48ad9c9e9b650fabb12d899d2225b5
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-29 11:16:07 -0400

    code

--- main.ts
+++ main.ts
@@ -5,3 +5,73 @@
  * This program lights up neopixels to a number
 */
 
+// setup
+let count = 0
+let neopixelStrip: neopixel.Strip = null
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+
+// lights up an amount of neopixels based on the number
+input.onButtonPressed(Button.A, function() {
+    count = 4
+    while (count > 0) {
+        basic.pause(500)
+        
+        if (count = 4) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.show()
+            basic.pause(500)
+        }
+
+        if (count = 3) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.pause(500)
+        }
+
+        if (count = 2) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.pause(500)
+        }
+
+        if (count = 1) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.pause(500)
+        }
+
+        if (count = -1) {
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.show()
+            basic.pause(500)
+        }
+        neopixelStrip.show()
+        count = count - 1
+    }
+
+    basic.showIcon(IconNames.Happy)
+    
+})
+
